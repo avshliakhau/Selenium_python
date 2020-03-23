6d1a143897002ce51efb225f9553542036967fb1
Alex <igea@bk.ru>
2020-03-23 18:30:42 +0300

    Добавлены комментарии// Запускается в командной строке и в PyCharm // Описываю хронику

--- #2.py
+++ #2.py
@@ -2,13 +2,18 @@
 from selenium import webdriver
 import unittest
 
+# def is_alert_present(wd):
+#     try:
+#         wd.switch_to_alert().text
+#         return True
+#     except:
+#         return False
 
 class tast_add_grou(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
     def test_tast_add_group(self):
         wd = self.wd
         # open home page
@@ -40,14 +45,14 @@ class tast_add_grou(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
         # logout
         wd.find_element_by_link_text("Logout").click()
-    
+
     def is_element_present(self, how, what):
         try:
             self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e:
             return False
         return True
-    
+
     def is_alert_present(self):
         try:
             self.wd.switch_to_alert()
@@ -55,9 +60,9 @@ class tast_add_grou(unittest.TestCase):
             return False
         return True
 
-
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
