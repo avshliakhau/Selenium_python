c6e932ab19fffbbe234b7f1459ca07293f2dce8e
Alex <igea@bk.ru>
2020-03-26 19:34:04 +0300

    Конструктор класса

---
+++ contact.py
@@ -0,0 +1,12 @@
+class Contact:
+
+    def __init__(self, firstname, lastname, address, mobile, email, bday, bmonth, byear):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+
