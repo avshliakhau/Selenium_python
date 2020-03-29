ef1090b288d43a5c46e3c99c414595ab0ee83705
Alex <igea@bk.ru>
2020-03-29 22:57:20 +0300

    создание помощника GroupHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
 
 class Application:
 
@@ -7,6 +9,7 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -23,27 +26,27 @@ class Application:
     #     wd.find_element_by_name("pass").send_keys(password)
     #     wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
+    # def open_groups_page(self):
+    #     wd = self.wd
+    #     wd.find_element_by_link_text("groups").click()
+    #
+    # def create_group(self, group):
+    #     wd = self.wd
+    #     self.open_groups_page()
+    #     wd.find_element_by_name("new").click()
+    #     wd.find_element_by_name("group_name").click()
+    #     wd.find_element_by_name("group_name").clear()
+    #     wd.find_element_by_name("group_name").send_keys(group.name)
+    #     wd.find_element_by_name("group_header").clear()
+    #     wd.find_element_by_name("group_header").send_keys(group.header)
+    #     wd.find_element_by_name("group_footer").clear()
+    #     wd.find_element_by_name("group_footer").send_keys(group.footer)
+    #     wd.find_element_by_name("submit").click()
+    #     self.return_to_groups_page()
+    #
+    # def return_to_groups_page(self):
+    #     wd = self.wd
+    #     wd.find_element_by_link_text("groups").click()
 
     # def logout(self):
     #     wd = self.wd
--- test/test_add_group_v2.py
+++ test/test_add_group_v2.py
@@ -13,15 +13,15 @@ def app(request):
 
 def test_add_group1(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="Group #1", header="Header #1", footer="Footer #1"))
+    app.group.create(Group(name="Group #1", header="Header #1", footer="Footer #1"))
     app.session.logout()
 
 def test_add_group2(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="Group #2", header="Header #2", footer="Footer #2"))
+    app.group.create(Group(name="Group #2", header="Header #2", footer="Footer #2"))
     app.session.logout()
 
 def test_add_group3(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="Group #3", header="Header #3", footer="Footer #3"))
+    app.group.create(Group(name="Group #3", header="Header #3", footer="Footer #3"))
     app.session.logout()
