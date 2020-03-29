19debd0236149b1013b5e7bebf47d9dfe341ed00
Alex <igea@bk.ru>
2020-03-29 12:54:41 +0300

    небольшое улучшение читаемости тестов

--- test_add_group_v2.py
+++ test_add_group_v2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*--
 from selenium import webdriver
 from group import Group
-import unittest
+import unittest, time
 
 class tast_add_group(unittest.TestCase):
     def setUp(self):
@@ -13,6 +13,7 @@ class tast_add_group(unittest.TestCase):
 
     def login(self, wd, username, password):
         # login-user
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -26,6 +27,7 @@ class tast_add_group(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -38,6 +40,7 @@ class tast_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def return_to_groups_page(self, wd):
         # return to groups page
@@ -47,24 +50,25 @@ class tast_add_group(unittest.TestCase):
         # logout
         wd.find_element_by_link_text("Logout").click()
 
-    def test_add_group(self):
+    def test_add_group1(self):
         wd = self.wd
-        self.open_home_page(wd)
+        # self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
+        # self.open_groups_page(wd)
         self.create_group(wd, Group(name="Group #1", header="Header #1", footer="Footer #1"))
-        self.return_to_groups_page(wd)
+        # self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_group2(self):
         wd = self.wd
-        self.open_home_page(wd)
+        # self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
+        # self.open_groups_page(wd)
         self.create_group(wd, Group(name="Group #2", header="Header #2", footer="Footer #2"))
-        self.return_to_groups_page(wd)
+        # self.return_to_groups_page(wd)
         self.logout(wd)
 
+    # time.sleep(10)
     def tearDown(self):
         self.wd.quit()
 
