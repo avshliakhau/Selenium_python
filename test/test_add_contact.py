b8f9251d91403ff1be29ab5a76beb23c6d30d9b6
Alex <igea@bk.ru>
2020-03-31 19:16:54 +0300

    тесты добавления групп и контактов (по 3 шт.)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from model.contact import Contact
 from fixture.application import Application
+from model.contact import Contact
 
 
 @pytest.fixture
@@ -10,15 +10,18 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-def test_add_contact1(self):
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Kol", lastname="Pol", address="Minsk, Jukova str. 4",
-                                    mobile="+375296507090", email="serge35@inbox.ru", bday="19", bmonth="June", byear="1995"))
-    app.logout()
+def test_add_contact1(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create_contact(Contact(firstname="Kol", lastname="Pol", address="Minsk, Jukova str. 4", mobile="+375296507090", email="serge35@inbox.ru", byear="1995"))
+    app.session.logout()
 
-# def test_add_contact2(self):
-#     app.login(username="admin", password="secret")
-#     app.create_contact(Contact(firstname="Maks", lastname="Second", address="Brest, Moskowskay str. 55",
-#                                         mobile="+375336007000", email="ser35@bk.ru", bday="22", bmonth="July", byear="1990"))
-#     app.logout()
+def test_add_contact2(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create_contact(Contact(firstname="Maks", lastname="Second", address="Brest, Moskowskay str. 55", mobile="+375336007000", email="ser35@bk.ru", byear="1990"))
+    app.session.logout()
+
+def test_add_contact3(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create_contact(Contact(firstname="Ilon", lastname="Mask", address="Frisko, 158-str. 12B", mobile="+380356008899", email="ser35@tesla.us", byear="1981"))
+    app.session.logout()
 #
--- test/test_add_group_v2.py
+++ test/test_add_group_v2.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*--
 import pytest
 from fixture.application import Application
-from model.contact import Contact
 from model.group import Group
+from model.contact import Contact
 
 
 @pytest.fixture
@@ -12,32 +12,33 @@ def app(request):
     return fixture
 
 
-def test_add_group1(app):
+def test_add_group_1(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Group #1", header="Header #1", footer="Footer #1"))
     app.session.logout()
 
-def test_add_group2(app):
+def test_add_group_2(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Group #2", header="Header #2", footer="Footer #2"))
     app.session.logout()
 
-def test_add_group3(app):
+def test_add_group_3(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Group #3", header="Header #3", footer="Footer #3"))
     app.session.logout()
 
-def test_add_contact1(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.create_contact(Contact(firstname="Kol", lastname="Pol", address="Minsk, Jukova str. 4", mobile="+375296507090", email="serge35@inbox.ru", byear="1995"))
-    app.session.logout()
-
-def test_add_contact2(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.create_contact(Contact(firstname="Maks", lastname="Second", address="Brest, Moskowskay str. 55", mobile="+375336007000", email="ser35@bk.ru", byear="1990"))
-    app.session.logout()
-
-def test_add_contact3(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.create_contact(Contact(firstname="Ilon", lastname="Mask", address="Frisko, 158-str. 12B", mobile="+380356008899", email="ser35@tesla.us", byear="1981"))
-    app.session.logout()
+# def test_add_contact1(app):
+#     app.session.login(username="admin", password="secret")
+#     app.contact.create_contact(Contact(firstname="Kol", lastname="Pol", address="Minsk, Jukova str. 4", mobile="+375296507090", email="serge35@inbox.ru", byear="1995"))
+#     app.session.logout()
+#
+# def test_add_contact2(app):
+#     app.session.login(username="admin", password="secret")
+#     app.contact.create_contact(Contact(firstname="Maks", lastname="Second", address="Brest, Moskowskay str. 55", mobile="+375336007000", email="ser35@bk.ru", byear="1990"))
+#     app.session.logout()
+#
+# def test_add_contact3(app):
+#     app.session.login(username="admin", password="secret")
+#     app.contact.create_contact(Contact(firstname="Ilon", lastname="Mask", address="Frisko, 158-str. 12B", mobile="+380356008899", email="ser35@tesla.us", byear="1981"))
+#     app.session.logout()
+#
