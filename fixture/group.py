02ce590a37a56ee6e5a40384d62a6fabfb53379e
Alex <igea@bk.ru>
2020-04-06 12:48:09 +0300

    изменения группы и контакты (задание №10 3_5)

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
         wd = self.app.wd
@@ -34,12 +35,8 @@ class GroupHelper:
     def form(self, group):
         wd = self.app.wd
         self.change_field_value("group_name", group.name)
-        # time.sleep(3)
         self.change_field_value("group_header", group.header)
-        # time.sleep(3)
         self.change_field_value("group_footer", group.footer)
-        # time.sleep(3)
-
 
     def edit_group(self, group):
         wd = self.app.wd
