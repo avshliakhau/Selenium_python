b1f6f485b40ad186542bd5b94a5c0460b4ab1088
Alex <igea@bk.ru>
2020-04-06 11:32:57 +0300

    изменения группы (задание №9 3_4)

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*--
-
+from model.group import Group
+import time
 
 def test_delete_first_group(app):
     # app.session.login(username="admin", password="secret")
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
+        # time.sleep(5)
     app.group.delete_first_group()
     # app.session.logout()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,12 +1,19 @@
 from model.group import Group
+import time
 
 
 def test_edit_group1(app):
     # app.session.login(username="admin", password="secret")
-    app.group.edit_group(Group(name="NewGroupEdit"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
+        time.sleep(2)
+    app.group.edit_group(Group(name="NewGroupEdit", footer="NewFooterEdit"))
     # app.session.logout()
 
 def test_edit_group2(app):
     # app.session.login(username="admin", password="secret")
-    app.group.edit_group(Group(header="NewHeaderEdit", footer="NewFooterEdit"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
+        time.sleep(2)
+    app.group.edit_group(Group(name="Edit_NameGroup", header="Group_Edit", footer="Footer_Edit"))
     # app.session.logout()
