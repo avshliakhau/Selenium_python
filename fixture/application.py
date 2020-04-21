395a0f8946f1e3c031f2d2b70741b06d7ac23a8a
Alex <igea@bk.ru>
2020-04-22 00:50:12 +0300

    опция командной строки (задание №16)

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,12 +6,21 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,8 +31,9 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not (wd.current_url == ("http://localhost/addressbook/") and len(wd.find_elements_by_xpath("//input[@type='button']")) > 1):
-            wd.get("http://localhost/addressbook/")
+        if not (wd.current_url == (self.base_url) and len(wd.find_elements_by_xpath("//input[@type='button']")) > 1):
+            wd.get(self.base_url)
+            # wd.get("http://localhost/addressbook/")
         # if not (wd.current_url.startswith("http://localhost/addressbook/") and len(wd.find_elements_by_xpath("//input[@type='button']")) > 1):
         #     wd.get("http://localhost/addressbook/")
 
