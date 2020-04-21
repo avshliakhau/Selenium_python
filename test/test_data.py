c7b75d93a145b5d40eeaacc59630c6cf5518e50f
Alex <igea@bk.ru>
2020-04-21 11:57:11 +0300

    тесты для всех данных очищен (задание №14)

--- test/test_data.py
+++ test/test_data.py
@@ -14,12 +14,6 @@ def test_data_on_home_page(app):
     assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
-# def test_email_on_contact_view_page(app):# Страница редактирования контакта
-#     contact_from_view_page = app.contact.get_contact_from_view_page(0) # Details страница с деталями контакта
-#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0) # Edit Страница редактирования
-#     assert contact_from_view_page.email == contact_from_edit_page.email
-#     assert contact_from_view_page.email2 == contact_from_edit_page.email2
-#     assert contact_from_view_page.email3 == contact_from_edit_page.email3
 
 def clear(s):
     return re.sub("[() /-]", "", s) # !!!то что надо вырезать обязательно в квадр скобках, иначе падает!!!
