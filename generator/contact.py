65c23c6656394150fade398e1e4200d83a164781
Alex <igea@bk.ru>
2020-04-27 22:42:26 +0300

    поправил, jsonpickle работает (задание №18)

--- generator/contact.py
+++ generator/contact.py
@@ -45,4 +45,5 @@ testdata = [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    out.write(jsonpickle.dumps(testdata, indent=2))
+    # out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
