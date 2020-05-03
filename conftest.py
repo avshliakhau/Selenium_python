831dc5f447f7014b7fbf0686af76c87d5e1fbee2
Alex <igea@bk.ru>
2020-05-03 18:31:14 +0300

    ошибка MaxRetryError

--- conftest.py
+++ conftest.py
@@ -4,38 +4,44 @@ import jsonpickle
 import os.path
 import importlib
 from fixture.application import Application
+from fixture.db import DbFixture
 
 fixture = None
 target = None
 
-# @pytest.fixture
-# def app(request):
-#     global fixture
-#     browser = request.config.getoption("--browser")
-#     base_url = request.config.getoption("--baseUrl")
-#     if fixture is None:
-#         fixture = Application(browser=browser, base_url=base_url)
-#     else:
-#         if not fixture.is_valid:
-#             fixture = Application(browser=browser, base_url=base_url)
-#     fixture.session.ensure_login(username="admin", password="secret")
-#     # fixture.session.login(username="admin", password="secret")
-#     return fixture
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
 
 @pytest.fixture
 def app(request):
     global fixture
-    global target
+    # global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                   request.config.getoption("--target"))
-        with open(config_file) as f:
-            target = json.load(f)
+    web_config = load_config(request.config.getoption("--target"))["web"]
+    # browser = request.config.getoption("--browser") # вынесли в load_config
+    # if target is None:
+    #     config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+    #                                request.config.getoption("--target"))
+    #     with open(config_file) as f:
+    #         target = json.load(f)
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=target["baseUrl"])
-    fixture.session.ensure_login(username=target["username"], password=target["password"])
+        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
+
+@pytest.fixture(scope="session")# add 7_2
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+    def fin():
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
@@ -67,3 +73,18 @@ def load_from_module(module):
 def load_from_json(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
+
+
+# @pytest.fixture
+# def app(request):
+#     global fixture
+#     browser = request.config.getoption("--browser")
+#     base_url = request.config.getoption("--baseUrl")
+#     if fixture is None:
+#         fixture = Application(browser=browser, base_url=base_url)
+#     else:
+#         if not fixture.is_valid:
+#             fixture = Application(browser=browser, base_url=base_url)
+#     fixture.session.ensure_login(username="admin", password="secret")
+#     # fixture.session.login(username="admin", password="secret")
+#     return fixture
