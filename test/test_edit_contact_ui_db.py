848be9141a9f21f9ec1f4fe774514cf03bcf7585
Alex <igea@bk.ru>
2020-05-07 19:29:21 +0300

    ошибка  "missing 1 required positional argument" (задание №20)

---
+++ test/test_edit_contact_ui_db.py
@@ -0,0 +1,16 @@
+from model.contact import Contact
+import random
+
+
+def test_edit_some_contact(app, db, check_ui):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create_contact(Contact(firstname="test_create", lastname="Test_Create"))
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    app.contact.edit_contact_by_id(contact.id)
+    new_contacts = db.get_contact_list()
+    assert old_contacts == new_contacts
+
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == \
+               sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
